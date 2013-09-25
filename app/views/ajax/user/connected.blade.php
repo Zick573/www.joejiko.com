c8d7d912ed41b3b379098844824cff6ade69c3fe
Joe Jiko <me@joejiko.com>
2013-09-24 20:47:50 -0400

    disconnect view update

--- app/views/ajax/user/connected.blade.php
+++ app/views/ajax/user/connected.blade.php
@@ -2,7 +2,6 @@
 @section('content')
     @if(isset($user))
         <h1>You're connected!</h1>
-        <p>Do you want to <a class="btn-user-disconnect" href="/user/disconnect">disconnect</a>?</p>
     @else
         <h1>Something went wrong</h1>
         <p>I don't even know..</p>
