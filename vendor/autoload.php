459f1c8d729de95d5e5ede5ea91468b312d17488
Joe Jiko <me@joejiko.com>
2013-09-25 00:51:20 -0400

    nightly

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd43aa48ce5d4d11d68c4343053b085fc::getLoader();
+return ComposerAutoloaderInit585aa386408048ac27cc26bc8a3163c3::getLoader();
