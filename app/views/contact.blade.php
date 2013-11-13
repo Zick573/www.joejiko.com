6f049c26e6c08a872e3c804f2e71ade97a9d2bcc
Joe Jiko <me@joejiko.com>
2013-11-13 00:44:23 -0500

    update contact copy

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -8,7 +8,7 @@
   <h2>Not ready yet</h2>
   <p>I'm making something cool to go here. But for now.. The best way to contact me is by using Google+ hangouts</p>
   <p><a href="http://jiko.us/XHoGuc">Add me to your circles</a> and send a message</a></p>
-  <p>or view my <a href="/contact/other">other profiles</a></p>
+  <p>If you're against Google+ or something, you can try one of my <a href="/contact/other">other profiles</a></p>
   <p><small>(yes, this is a test)</small></p>
 </article>
 @stop
