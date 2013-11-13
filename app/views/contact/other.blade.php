05d340f3bcc80d62c9ffcaaa2297c8674fd1eeed
Joe Jiko <me@joejiko.com>
2013-11-13 00:45:14 -0500

    update other contact dom

--- app/views/contact/other.blade.php
+++ app/views/contact/other.blade.php
@@ -3,17 +3,19 @@
   Other contact information and social profiles
 @stop
 @section('content')
-  <h1>Contact</h1>
-  <h2>Other methods</h2>
-  <h3>Social profiles</h3>
-  <ul>
-    <li><a href="https://plus.google.com/110880509059057751100/posts" target="_blank">Google+</a></li>
-    <li><a href="https://github.com/JoeJiko">GitHub</a></li>
-    <li><a href="https://twitter.com/joejiko" target="_blank">Twitter</a></li>
-    <li><a href="http://instagram.com/joejiko" target="_blank">Instagram</a></li>
-    <li><a href="http://www.linkedin.com/in/joejiko" target="_blank">LinkedIn</a></li>
-    <li><a href="http://pinterest.com/joejiko/boards/" target="_blank">Pinterest</a></li>
-    <li><a href="https://www.facebook.com/joejiko" target="_blank">Facebook (art page)</a></li>
-    <li><a href="https://www.facebook.com/joejiko" target="_blank">Facebook (profile)</a></li>
-  </ul>
+  <article class="base-article">
+    <h1>Contact</h1>
+    <h2>Other methods</h2>
+    <h3>Social profiles</h3>
+    <ul>
+      <li><a href="https://plus.google.com/110880509059057751100/posts" target="_blank">Google+</a></li>
+      <li><a href="https://github.com/JoeJiko">GitHub</a></li>
+      <li><a href="https://twitter.com/joejiko" target="_blank">Twitter</a></li>
+      <li><a href="http://instagram.com/joejiko" target="_blank">Instagram</a></li>
+      <li><a href="http://www.linkedin.com/in/joejiko" target="_blank">LinkedIn</a></li>
+      <li><a href="http://pinterest.com/joejiko/boards/" target="_blank">Pinterest</a></li>
+      <li><a href="https://www.facebook.com/joejiko" target="_blank">Facebook (art page)</a></li>
+      <li><a href="https://www.facebook.com/joejiko" target="_blank">Facebook (profile)</a></li>
+    </ul>
+  </article>
 @stop
