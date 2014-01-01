58256f3c4b4bc9e07d54e6226be89094e6c7ee44
Jiko <me@joejiko.com>
2013-12-31 23:53:09 -0500

    add twitter card meta

--- app/controllers/archive/TwitterArchiveController.php
+++ app/controllers/archive/TwitterArchiveController.php
@@ -6,6 +6,10 @@ class TwitterArchiveController extends \DefaultController {
   {
     if(Input::has('q')) return $this->searchQuery(Input::get('q'));
 
+    if(Input::has('screen_name')) return $this->searchScreenName(Input::get('screen_name'));
+
+    if(Input::has('show')) return $this->showMentions();
+
     $result = DB::table('twitter_archive')
       ->orderby('timestamp', 'desc')
       ->take(250)
@@ -34,6 +38,56 @@ class TwitterArchiveController extends \DefaultController {
     LIMIT 10";
   }
 
+  public function searchById($id)
+  {
+
+  }
+
+  public function showMentions()
+  {
+    $result = DB::table('twitter_archive')
+      ->whereRaw("`text` LIKE '%@%'")
+      ->whereRaw(DB::raw("str_to_date(`timestamp`, '%Y-%m-%d %H:%i:%s+0000') BETWEEN '2013-01-01 00:00:00' AND '2013-12-31 00:00:00'"))
+      ->get();
+    $mentions = [];
+
+    foreach($result as $tweet) {
+      preg_match("/(?<=^|\s)@([a-z0-9_]+)/i", $tweet->text, $matches);
+      // hash tags
+      // preg_match("$(?<=^|(?<=[^a-zA-Z0-9-\.]))#([A-Za-z]+[A-Za-z0-9-]+)$", $tweet->text, $matches);
+      if(count($matches)) {
+        $mentions[] = $matches[0];
+      }
+    }
+
+    $count = [];
+
+    foreach($mentions as $user) {
+      if(!array_key_exists($user, $count)):
+        $count[$user] = 1;
+        continue;
+      endif;
+
+      $count[$user]++;
+    }
+    arsort($count);
+    var_dump($count);
+    exit();
+
+    $html = '<ul class="tweets">';
+    foreach($result as $row) {
+      $html .= '<li class="tweet">'
+      . '<span class="col text">'.$row->text.'</span><!--'
+      . '--><a class="col link-tweet" href="http://twitter.com/JoeJiko/status/'.$row->tweet_id.'">'
+      . '<time class="timestamp">'.date("Y-m-d", strtotime($row->timestamp)).'</time>'
+      . '</a>'
+      . '</li>';
+    }
+    $html .= '<li class="tweet end-of-tweets">Older tweets are currently unavailable.. trying <a href="#top">searching</a> instead?';
+    $html .= '</ul>';
+    return View::make('archive.index')->withContent($html);
+  }
+
   public function searchQuery($text, $html='')
   {
     if(Input::has('sort')):
@@ -42,7 +96,9 @@ class TwitterArchiveController extends \DefaultController {
         ->orderBy( DB::raw("str_to_date(`timestamp`, '%Y-%m-%d %H:%i:%s+0000')"), 'asc')
         ->get();
     else:
-      $result = DB::table('twitter_archive')->whereRaw("MATCH(text) AGAINST ('".$text."')")->get();
+      $result = DB::table('twitter_archive')
+        ->whereRaw("MATCH(text) AGAINST ('".$text."')")
+        ->get();
     endif;
     $html .= '<header class="content-header"><h1 class="view-title">Twitter archive</h1>'
     . '<h2 class="info-matches">'.count($result).' matches for <em>'.$text.'</em>&nbsp;</h2>'
