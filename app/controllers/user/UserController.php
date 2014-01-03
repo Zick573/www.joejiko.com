7dab5e551931ab88134d5f5c27c8b9ef0e5d7683
Joe <joea@postcardmania.com>
2014-01-03 17:49:39 -0500

    debug

--- app/controllers/user/UserController.php
+++ app/controllers/user/UserController.php
@@ -129,17 +129,7 @@ class UserController extends DefaultController {
   public function OAuthRegister(Hybrid_Provider_Adapter $provider, Hybrid_User_Profile $profile)
   {
 
-    $user_missing_email = isset($profile->email) ? true : false;
-
-    if( empty(trim($profile->email)) ) {
-
-      /**
-       * @todo store this user as not active
-       * until they add & verify an email.
-       */
-      $user_missing_email = true;
-
-    }
+    $user_missing_email = (isset($profile->email) && !empty($profile->email));
 
     if(!$user_missing_email) {
 
